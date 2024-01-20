27785138a89e989a7fe084bfb65f29a066afdaf9
jgjgill <dbdltm22@naver.com>
2024-01-20 22:49:26 +0900

    feat: styleX 관련 설정 진행 라이브러리 및 관련 플러그인 설치 Next.js 환경에서의 세팅 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import * as stylex from "@stylexjs/stylex";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "dnd 10th-1 fe",
@@ -11,8 +13,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html {...stylex.props(styles.reset)} lang="ko">
       <body>{children}</body>
     </html>
   );
 }
+
+const styles = stylex.create({
+  reset: {
+    minHeight: "100%",
+    margin: 0,
+    padding: 0,
+  },
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import * as stylex from "@stylexjs/stylex";
+
 export default function Home() {
-  return <main>dnd-10-1 fe</main>;
+  return (
+    <main>
+      <button {...stylex.props(styles.button)}>dnd-10-1 fe</button>
+    </main>
+  );
 }
+
+const styles = stylex.create({
+  button: {
+    background: "teal",
+  },
+});
