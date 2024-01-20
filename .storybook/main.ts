c880d0f757dc3f839dc72a3a83b7b2e6f5608684
jgjgill <dbdltm22@naver.com>
2024-01-21 01:15:50 +0900

    feat: Storybook 관련 설정 진행 Storybook, Chromatic 환경 구성 예제 코드 및 스니펫 추가

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../"),
+      };
+    }
+
+    return config;
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
