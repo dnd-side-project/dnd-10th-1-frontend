ca307b1853623036419eb7fb733a14e40c137df2
jgjgill <dbdltm22@naver.com>
2024-01-25 23:17:40 +0900

    fix: 스토리북 관련 에러 해결 경로 및 tailwind 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,7 +17,7 @@ const config: StorybookConfig = {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        "@": path.resolve(__dirname, "../"),
+        "@": path.resolve(__dirname, "../src/"),
       };
     }
 
