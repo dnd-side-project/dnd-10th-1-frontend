9b648982d3bb8ca91c68731db2d2558c9ae72c7e
jgjgill <dbdltm22@naver.com>
2024-01-25 22:03:43 +0900

    fix: MSW 관련 경로 수정

--- src/__mock__/handler/client/temp-client-handler.ts
+++ src/__mock__/handler/client/temp-client-handler.ts
@@ -1,11 +1,11 @@
 import { http, type HttpHandler, HttpResponse } from "msw"
 
 import { tempMock } from "@/__mock__/data/temp-mock"
-import { BACK_URL, END_POINT } from "@/constants/apis"
+import { CLIENT_URL, END_POINT } from "@/constants/apis"
 
 const tempClientHandler: HttpHandler[] = [
   //   getTemp
-  http.get(BACK_URL + END_POINT.temp, () => {
+  http.get(CLIENT_URL + END_POINT.temp, () => {
     return HttpResponse.json(tempMock)
   }),
 ]
