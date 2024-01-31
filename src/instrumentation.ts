3f5cdc7c09193fb8f0553420d7800187440f3fc4
jgjgill <79239852+jgjgill@users.noreply.github.com>
2024-01-31 20:46:12 +0900

    chore: 안쓰는 파일 삭제

--- src/instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { serverWorker } = await import("./__mock__/handler/server/index")
-    serverWorker.listen()
-  }
-}
