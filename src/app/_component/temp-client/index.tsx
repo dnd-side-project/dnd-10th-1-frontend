063b57a9c5038dcaf084e605a5a83920089052af
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-02 04:32:08 +0900

    refactor: button 경로수정

--- src/app/_component/temp-client/index.tsx
+++ src/app/_component/temp-client/index.tsx
@@ -4,6 +4,5 @@ import { useGetUsers } from "@/services/temp/hook"
 
 export default function TempClient() {
   const { data: user } = useGetUsers()
-  console.log(process.env.NEXT_PUBLIC_API_MOCKING)
   return <div className="temp-btn">{JSON.stringify(user?.message)}</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Button from "@/components/button"
+import { Button } from "@/components/button"
 
 import TempClient from "./_component/temp-client"
 
