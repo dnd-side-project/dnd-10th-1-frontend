1bf1dfa7d35fd176ba5adf44df5e42d20ab2715c
jgjgill <dbdltm22@naver.com>
2024-02-06 00:01:37 +0900

    fix: 스플래시 관련 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 import { Button } from "@/components/button"
 import Splash from "@/components/splash"
 import { cn } from "@/libs/tailwind/cn"
 
-export default async function Home() {
+export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500)
+  }, [])
+
   return (
-    <main>
-      <Splash />
-      <Button className={cn("t2 bg-primary-3-200")}>dnd-10-1 fe</Button>
+    <main className="relative">
+      {loading ? (
+        <Splash />
+      ) : (
+        <>
+          <Button className={cn("t2 bg-primary-3-200")}>dnd-10-1 fe</Button>
+        </>
+      )}
     </main>
   )
 }
--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -1,23 +1,10 @@
-"use client"
-
 import Image from "next/image"
-import { useEffect, useState } from "react"
 
 import SplashLogo from "@/app/assets/images/splash-logo.png"
 
 export default function Splash() {
-  const [show, setShow] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShow(false)
-    }, 1000)
-  }, [])
-
-  if (!show) return null
-
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex h-screen items-center justify-center bg-[#4747F4]">
+    <div className="flex h-screen items-center justify-center bg-[#4747F4]">
       <Image src={SplashLogo} alt="스플래시 로고" priority className="animate-bounce" />
     </div>
   )
