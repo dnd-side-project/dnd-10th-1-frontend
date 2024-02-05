5f48971dac9681d7496382d4b0ef55789c5c3c78
jgjgill <dbdltm22@naver.com>
2024-02-05 23:42:11 +0900

    feat: 스플래쉬 화면 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { Button } from "@/components/button"
+import Splash from "@/components/splash"
 import { cn } from "@/libs/tailwind/cn"
-
-import TempClient from "./_component/temp-client"
-
-export const dynamic = "force-dynamic"
 
 export default async function Home() {
   return (
     <main>
-      <TempClient />
+      <Splash />
       <Button className={cn("t2 bg-primary-3-200")}>dnd-10-1 fe</Button>
     </main>
   )
