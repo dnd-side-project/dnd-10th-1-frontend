fc0961cf13eb283e8e72b8aa522aa2dc77a3d1e5
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-10 21:20:12 +0900

    refactor: drawer style을 고정한다.

--- src/components/drawer/drawer.stories.tsx
+++ src/components/drawer/drawer.stories.tsx
@@ -1,16 +1,8 @@
+"use client"
+
 import type { Meta, StoryObj } from "@storybook/react"
 
-import { Button } from "../button"
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "."
+import { Drawer, DrawerClose, DrawerContent, DrawerTrigger } from "."
 
 const meta: Meta<typeof Drawer> = {
   title: "Components/Drawer",
@@ -24,22 +16,11 @@ type Story = StoryObj<typeof Drawer>
 export const Primary: Story = {
   render: ({ ...args }) => (
     <Drawer {...args}>
-      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerTrigger asChild>
+        <button>Open Drawer</button>
+      </DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
-        </DrawerHeader>
-        <DrawerFooter>
-          <DrawerClose>
-            <Button variant="outline" className="border-blue-200">
-              Submit
-            </Button>
-          </DrawerClose>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
+        <DrawerClose>닫아주세요</DrawerClose>
       </DrawerContent>
     </Drawer>
   ),
