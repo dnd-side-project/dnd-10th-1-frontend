e2f89b1f5518ad45c9861d8b753141bf06a23265
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-12 00:44:12 +0900

    feat: font를 추가한다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import "./globals.css"
-
-import { Inter } from "next/font/google"
-
-const inter = Inter({ subsets: ["latin"] })
-
 import "@stackflow/plugin-basic-ui/index.css"
 
 import type { Metadata } from "next"
 
 import Providers from "@/components/provider"
 import { cn } from "@/libs/tailwind/cn"
+import { pretendard } from "@/styles/fonts/pretendard"
 
 export const metadata: Metadata = {
   title: "dnd 10th-1 fe",
@@ -23,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={cn(inter.className, "flex justify-center bg-gray-300")}>
+      <body className={cn(pretendard.className, "flex justify-center bg-gray-300")}>
         <Providers>
           <div className={cn("relative min-h-screen w-full max-w-md  overflow-hidden")}>{children}</div>
         </Providers>
