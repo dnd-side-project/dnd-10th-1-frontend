eb0d5cacf0bba3fea3b62c05294cd0fdab7057f1
Zero <iolpd42@gmail.com>
2024-02-12 16:19:03 +0900

    feat: #9 font를 추가한다 (#10)
    * feat: font를 추가한다
    
    * refactor: decorators로 직접 구현

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react"
 import "../src/app/globals.css"
+import { pretendard } from "../src/styles/fonts/pretendard"
+import React from "react"
 
 const preview: Preview = {
   parameters: {
@@ -15,6 +17,13 @@ const preview: Preview = {
       appDirectory: true,
     },
   },
+  decorators: [
+    Story => (
+      <main className={pretendard.className}>
+        <Story />
+      </main>
+    ),
+  ],
 }
 
 export default preview
