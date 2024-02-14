e3570a48175c3c56dc5e1933ebe4dec8e7000771
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-15 03:25:37 +0900

    feat: 확인을 위한 loadingbar, header-nav-bar 추가

---
+++ src/components/framer-motion/loadingbar/loadingbar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import Loadingbar from "./loadingbar"
+
+const meta: Meta<typeof Loadingbar> = {
+  title: "Motion/Loadingbar",
+  component: Loadingbar,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof Loadingbar>
+
+export const Primary: Story = {
+  render: () => (
+    <div className="phone-size bg-gray-950">
+      <Loadingbar />,
+    </div>
+  ),
+}
