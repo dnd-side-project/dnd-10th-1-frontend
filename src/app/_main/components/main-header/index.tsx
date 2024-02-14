fa30af8d5064c55a6880055d63955d6b419121e6
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-14 16:27:14 +0900

    feat: 메인 로고, 이미지 추가 완료

--- src/app/_main/components/main-header/index.tsx
+++ src/app/_main/components/main-header/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 
+import characterLogo from "@/assets/images/main-character-logo.png"
+import mainLogo from "@/assets/images/splash-logo.png"
 import MainLinear from "@/assets/svgs/main-linear.svg"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar"
 type Props = {
@@ -9,19 +11,21 @@ type Props = {
 
 export default function MainHeader({ userNickName, userProfileImage }: Props) {
   return (
-    <header className="relative overflow-hidden rounded-b-[20px]">
-      <div className="relative z-10 flex h-60 justify-center bg-[#4747F4]">
-        <div className="bg relative w-full max-w-[360px]">
+    <header className="relative">
+      <div className="relative z-10 flex h-60 justify-center overflow-hidden rounded-b-[20px] bg-[#4747F4]">
+        <div className="bg relative w-full max-w-[360px] overflow-hidden rounded-b-[28px]">
+          <Image src={characterLogo} alt="EZPZ 캐릭터" />
+          <Image className="absolute left-[24px] top-[38px]" height={25} width={68} src={mainLogo} alt="Main 로고" />
           <Avatar className="absolute right-6 top-[34px] h-[34px] w-[34px]">
             <AvatarImage src={userProfileImage} />
             <AvatarFallback>{userNickName}</AvatarFallback>
           </Avatar>
           <div className="absolute bottom-[52px] left-6">
-            <span className="h2 whitespace-pre-wrap text-gray-100">{`${userNickName}님\n오늘도 얼음땡과\n함께해요! 💗`}</span>
+            <span className="h2 whitespace-pre-wrap leading-tight text-gray-100">{`${userNickName}님\n오늘도 얼음땡과\n함께해요! 💗`}</span>
           </div>
         </div>
       </div>
-      <Image className="absolute bottom-0 z-10 w-full" src={MainLinear} alt="Linear" />
+      <Image className="absolute bottom-0 z-10 h-[54px] w-full" src={MainLinear} alt="Linear" />
     </header>
   )
 }
