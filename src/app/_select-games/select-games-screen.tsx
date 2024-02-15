b8573f0faa125910a5102a3472facc3075a5b5f3
jgjgill <79239852+jgjgill@users.noreply.github.com>
2024-02-15 17:49:57 +0900

    feat: #18 게임 선택 화면 작업을 진행합니다. (#21)
    * feat: 게임 선택 이미지 추가
    
    * feat: 게임 선택 화면 구성
    
    * style: 게임 선택 화면 스토리 구성

---
+++ src/app/_select-games/select-games-screen.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image"
+import { useState } from "react"
+
+import BlankGame from "@/assets/svgs/select-games/blank-game.png"
+import MbtiGame from "@/assets/svgs/select-games/mbti-game.png"
+import AppBar from "@/components/app-bar"
+import { Button } from "@/components/button"
+import { cn } from "@/libs/tailwind/cn"
+
+type Props = {
+  onCompleteClick: (selected: "mbti" | "blank") => void
+}
+
+export default function SelectGamesScreen({ onCompleteClick }: Props) {
+  const [selected, setSelected] = useState<"mbti" | "blank">()
+
+  const mbtiGameClassName = cn("flex h-[168px] w-full items-center justify-center rounded-[10px]", {
+    "bg-gradient-to-b from-[#2A61E7] to-[#FF7EFA]": selected === "mbti",
+  })
+
+  const blankGameClassName = cn("flex h-[168px] w-full items-center justify-center rounded-[10px]", {
+    "bg-gradient-to-b from-[#2A61E7] to-[#FF7EFA]": selected === "blank",
+  })
+
+  return (
+    <div className="min-h-full bg-gray-950">
+      <AppBar title="게임선택" />
+      <div className="flex flex-col px-[24px] pb-[24px] text-center text-gray-100">
+        <div className="mb-[49px]">
+          <h2 className="mb-2.5 text-[25px] font-bold">게임을 선택하세요</h2>
+
+          <p className="t2">팀원들과 진행하고 싶은 게임을</p>
+          <p className="t2">선택해주세요.</p>
+        </div>
+
+        <div className="mb-[149px] flex flex-col gap-5">
+          <div className={mbtiGameClassName}>
+            <Button
+              onClick={() => {
+                setSelected("mbti")
+              }}
+              className="relative h-[162px] w-[calc(100%-6px)]"
+            >
+              <Image priority fill src={MbtiGame} alt="MBTI 게임" />
+            </Button>
+          </div>
+
+          <div className={blankGameClassName}>
+            <Button
+              onClick={() => {
+                setSelected("blank")
+              }}
+              className="relative h-[162px] w-[calc(100%-6px)]"
+            >
+              <Image priority fill src={BlankGame} alt="빈칸주제 게임" />
+            </Button>
+          </div>
+        </div>
+        <Button
+          disabled={!selected}
+          onClick={() => {
+            if (!selected) return
+
+            onCompleteClick(selected)
+          }}
+          className="disabled:opacity-1 mt-auto w-full bg-primary-300 text-white disabled:bg-[#B7B7B7]"
+        >
+          선택완료
+        </Button>
+      </div>
+    </div>
+  )
+}
