e5b8401925f090e9520225ca295e1e15f0baa0d4
jgjgill <dbdltm22@naver.com>
2024-02-17 19:12:01 +0900

    feat: mbti 결과 헤더 관련 작업

---
+++ src/app/_mbti/_result/constants.ts
@@ -0,0 +1,1 @@
+export const TITLE_LIST = ["팀조합", "나의 결과"] as const
--- src/app/_mbti/_result/mbti-result.tsx
+++ src/app/_mbti/_result/mbti-result.tsx
@@ -1,16 +1,31 @@
 import { AppScreen } from "@stackflow/plugin-basic-ui"
 import { ActivityComponentType } from "@stackflow/react"
+import { useState } from "react"
 
 import { useFlow } from "../../stackflow"
+import { TITLE_LIST } from "./constants"
+import HeaderNavBar from "./header-nav-bar"
 
-const MbtiLoading: ActivityComponentType = () => {
+const MbtiResult: ActivityComponentType = () => {
   const { replace } = useFlow()
+
+  const [selectedTitle, setSelectedTitle] = useState<(typeof TITLE_LIST)[number]>(TITLE_LIST[0])
 
   return (
     <AppScreen>
-      <div>result</div>
+      <div className="min-h-full bg-gray-950">
+        <HeaderNavBar
+          selectedTitle={selectedTitle}
+          onClick={title => {
+            setSelectedTitle(title)
+          }}
+        />
+
+        {selectedTitle === "나의 결과" && <div className="text-gray-25">나는 나의 결과</div>}
+        {selectedTitle === "팀조합" && <div className="text-gray-25">나는 팀조합</div>}
+      </div>
     </AppScreen>
   )
 }
 
-export default MbtiLoading
+export default MbtiResult
