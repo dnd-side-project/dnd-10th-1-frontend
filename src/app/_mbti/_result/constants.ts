6319f2f249d608746cf2987865a0ea67fa7fa31a
jgjgill <dbdltm22@naver.com>
2024-02-17 20:41:53 +0900

    feat: 팀결과 화면 구성

--- src/app/_mbti/_result/constants.ts
+++ src/app/_mbti/_result/constants.ts
@@ -1,1 +1,1 @@
-export const TITLE_LIST = ["팀조합", "나의 결과"] as const
+export const TITLE_LIST = ["팀결과", "나의 결과"] as const
--- src/app/_mbti/_result/mbti-result.tsx
+++ src/app/_mbti/_result/mbti-result.tsx
@@ -2,13 +2,12 @@ import { AppScreen } from "@stackflow/plugin-basic-ui"
 import { ActivityComponentType } from "@stackflow/react"
 import { useState } from "react"
 
-import { useFlow } from "../../stackflow"
 import { TITLE_LIST } from "./constants"
 import HeaderNavBar from "./header-nav-bar"
+import MyResult from "./my-result"
+import TeamResult from "./team-result"
 
 const MbtiResult: ActivityComponentType = () => {
-  const { replace } = useFlow()
-
   const [selectedTitle, setSelectedTitle] = useState<(typeof TITLE_LIST)[number]>(TITLE_LIST[0])
 
   return (
@@ -21,8 +20,8 @@ const MbtiResult: ActivityComponentType = () => {
           }}
         />
 
-        {selectedTitle === "나의 결과" && <div className="text-gray-25">나는 나의 결과</div>}
-        {selectedTitle === "팀조합" && <div className="text-gray-25">나는 팀조합</div>}
+        {selectedTitle === "팀결과" && <TeamResult />}
+        {selectedTitle === "나의 결과" && <MyResult />}
       </div>
     </AppScreen>
   )
