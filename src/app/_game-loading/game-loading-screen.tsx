29e81135ff01084f5d5013cf992460edccb4f8fb
Jihyeong00 <iolpd42@gmail.com>
2024-02-17 12:07:41 +0900

    design: 글자 디자인 시스템 변경으로 인한 값 수정

---
+++ src/app/_game-loading/game-loading-screen.tsx
@@ -0,0 +1,11 @@
+export function GameLoadingScreen() {
+  return (
+    <div className="relative h-full w-full bg-primary-300">
+      <div className="max-w-[360px]">
+        <header>
+          <h2>방장이\n 게임을 고르는 중이에요</h2>
+        </header>
+      </div>
+    </div>
+  )
+}
