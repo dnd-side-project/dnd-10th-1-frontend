4d3bd73365651d63ea90686a97de905f30eee8ec
jgjgill <dbdltm22@naver.com>
2024-02-17 22:08:50 +0900

    refactor: 팀 결과 이미지 및 스타일 수정

--- src/app/_mbti/_result/team-result.tsx
+++ src/app/_mbti/_result/team-result.tsx
@@ -13,22 +13,25 @@ export default function TeamResult() {
       </div>
 
       <div className="flex flex-col items-center px-6">
-        <Image src={IdeaBank} alt="팀 결과 이미지" className="mb-[54px]" />
+        <Image src={IdeaBank} alt="팀 결과 이미지" priority className="mb-[54px]" />
 
-        <div className="flex flex-col gap-[13px]">
+        <div className="mb-[110px] flex flex-col gap-[13px]">
           <ul className="t3 flex w-[273px] list-disc flex-col gap-2 rounded-xl bg-gray-25 px-5 py-3 tracking-[-0.5px] text-gray-950">
             <li>{"직장 내 분위기메이커일 확률이 높아요."}</li>
             <li>{"적극적이고 주도적으로 업무를 진행해요."}</li>
             <li>{"모든 동료와 좋은 관계를 유지하는 비타민같은 존재!"}</li>
           </ul>
 
-          <div className="mb-[110px] flex w-[273px] gap-[18px] rounded-xl bg-gray-25 px-[23px] py-3">
-            <Image src={Planning} alt="팀 제안 이미지" />
+          <div className="flex w-[273px] gap-[18px] rounded-xl bg-gray-25 px-[23px] py-3">
+            <div className="flex items-center justify-center">
+              <Image src={Planning} alt="팀 제안 이미지" />
+            </div>
             <p className="t3 w-[145px] text-gray-950">{"계획을 세우고 일의 우선순위를 정해보는 것은 어떨까요?"}</p>
           </div>
-
-          <p className="t1 mb-[25px] text-center text-gray-400">꾹 눌러 이미지를 저장해주세요.</p>
         </div>
+
+        <p className="t1 mb-[25px] text-center text-gray-400">꾹 눌러 이미지를 저장해주세요.</p>
+
         <Button className="h4-bold mt-auto h-full w-full bg-primary-300 py-[12.5px] text-gray-25">
           테스트 결과 공유하기
         </Button>
