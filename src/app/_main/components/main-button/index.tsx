51c9b4acd51c89e2bf8168e516f1c4d0740edb4f
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-20 13:37:34 +0900

    refactor: button props 구조변경

--- src/app/_main/components/main-button/index.tsx
+++ src/app/_main/components/main-button/index.tsx
@@ -7,19 +7,17 @@ import MainCreate from "@/assets/svgs/main/main-create.svg"
 import { cn } from "@/libs/tailwind/cn"
 
 type Props = {
-  control: "create" | "invite"
+  isCreate: boolean
   className?: string
 } & ComponentPropsWithoutRef<"button">
 
-export default function MainButton({ control, children, className, ...props }: PropsWithChildren<Props>) {
+export default function MainButton({ isCreate, children, className, ...props }: PropsWithChildren<Props>) {
   const ButtonComponents = (() => {
-    if (control === "create") {
+    if (isCreate) {
       const svg = <Image src={MainCreate} alt="열림버튼" />
       const title = "방 만들기"
       return { svg, title }
-    }
-
-    if (control === "invite") {
+    } else {
       const svg = <PlusSquare size={26} />
       const title = "초대 코드 입력하기"
       return { svg, title }
@@ -28,22 +26,22 @@ export default function MainButton({ control, children, className, ...props }: P
 
   const buttonClassName = cn(
     "border-4 p-6 rounded-[18px] w-full",
-    { "shadow-blue text-[22px] bold bg-pink-300 border-primary-300": control === "create" },
-    { "py-[14px] t1 shadow-blue bg-primary-300 border-pink-300": control === "invite" },
+    { "shadow-blue text-[22px] bold bg-pink-300 border-primary-300": isCreate },
+    { "py-[14px] t1 shadow-blue bg-primary-300 border-pink-300": !isCreate },
     className,
   )
 
   const iconClassName = cn(
     "text-white flex items-center leading-none",
-    { "text-[20px] font-bold gap-3": control === "create" },
-    { "h4-bold gap-5": control === "invite" },
+    { "text-[20px] font-bold gap-3": isCreate },
+    { "h4-bold gap-5": !isCreate },
   )
 
   return (
     <button {...props}>
       <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.95 }} className={buttonClassName}>
         <div className={iconClassName}>
-          {ButtonComponents?.svg} <span className="leading-none">{ButtonComponents?.title}</span>
+          {ButtonComponents.svg} <span className="leading-none">{ButtonComponents.title}</span>
         </div>
         {children}
       </motion.div>
