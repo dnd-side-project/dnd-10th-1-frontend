63e088a9954df65f2e762a5e591e7b94ba5b6189
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-20 13:39:45 +0900

    fix: mock 데이터 수정으로 인한 일부코드 변경

--- src/app/apis/temp/route.ts
+++ src/app/apis/temp/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server"
 
-import { tempMock } from "@/seeds/temp-mock"
-
 export async function GET() {
-  return NextResponse.json(tempMock)
+  return NextResponse.json({ message: "success" })
 }
