ef48b29fbd5cbdb5bdb857b1b00c89bda55ce8be
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-20 13:32:15 +0900

    refactor: main-content 코드분리

---
+++ src/app/_main/components/main-content/index.tsx
@@ -0,0 +1,25 @@
+import { DrawerClose, DrawerContent } from "@/components/drawer"
+import { Input } from "@/components/input"
+
+type Props = {
+  inviteWithCode: (event: FormData) => void
+}
+
+export default function MainContent({ inviteWithCode }: Props) {
+  return (
+    <DrawerContent>
+      <form action={inviteWithCode} className="p-7">
+        <div>
+          <span className="text-[18px] font-bold">코드 입력하기</span>
+        </div>
+        <div>
+          <span className="text-4 font-medium">초대받은 코드를 입력해주세요</span>
+        </div>
+        <Input name="inviteCode" className="mt-5" control="main" placeholder="여기에 입력해주세요" />
+        <DrawerClose className="mt-6 w-full text-[18px] font-semibold">
+          <button className="h-[50px] w-full rounded-[10px] bg-primary-300 text-white">완료</button>
+        </DrawerClose>
+      </form>
+    </DrawerContent>
+  )
+}
