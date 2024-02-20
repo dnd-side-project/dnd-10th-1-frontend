e6ccc6d8ee41ab4b4e7f4ecbd8663f090f9bb895
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-02-20 13:24:03 +0900

    refactor: 목 데이터를 분리합니다.

---
+++ src/seeds/user-mock.ts
@@ -0,0 +1,40 @@
+import { UserWaitingType } from "@/app/_waiting/types/game"
+import { UserInfoType } from "@/types/user"
+
+export const mockUserInfo: UserInfoType = {
+  userNickName: "현아",
+  userProfileImage: "https://source.unsplash.com/random/?cat",
+}
+
+export const mockUserList: UserWaitingType[] = [
+  {
+    id: 1,
+    nickName: "얼음공주",
+    profileImage: "https://source.unsplash.com/random/?cat",
+    status: true,
+  },
+  {
+    id: 2,
+    nickName: "얼음공주",
+    profileImage: "https://source.unsplash.com/random/?cat",
+    status: true,
+  },
+  {
+    id: 3,
+    nickName: "얼음공주",
+    profileImage: "https://source.unsplash.com/random/?cat",
+    status: true,
+  },
+  {
+    id: 4,
+    nickName: "얼음공주",
+    profileImage: "https://source.unsplash.com/random/?cat",
+    status: true,
+  },
+  {
+    id: 5,
+    nickName: "얼음공주",
+    profileImage: "https://source.unsplash.com/random/?cat",
+    status: true,
+  },
+]
